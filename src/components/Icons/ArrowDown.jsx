daaa8c340160ed99e5624c52f55e4a0214433913
Dima <katorgin.dmitry@gmail.com>
2021-10-23 18:24:39 +0300

    edit ArrowDown

--- src/components/Icons/ArrowDown.jsx
+++ src/components/Icons/ArrowDown.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ArrowDown = () => {
 	return (
-		<svg viewBox="0 0 40 120" xmlns='http://www.w3.org/2000/svg'>
+		<svg viewBox="0 0 36 114" xmlns='http://www.w3.org/2000/svg'>
 			<g
 				stroke='#FFF'
 				strokeWidth='6'
